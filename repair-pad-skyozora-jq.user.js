3a39804ce29a93694340565de193109fbcb02aa4
枫谷剑仙 <mapaler@163.com>
2021-07-22 15:48:04 +0800

    改造血量显示

--- repair-pad-skyozora-jq.user.js
+++ repair-pad-skyozora-jq.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         智龙迷城战友网jQ修复
 // @namespace    http://www.mapaler.com/
-// @version      1.1
+// @version      1.2
 // @description  解决无翻墙情况下智龙迷城战友网无法展开详情问题
 // @author       Mapaler <mapaler@163.com>
 // @copyright    2019+, Mapaler <mapaler@163.com>
@@ -15,6 +15,34 @@
 
 (function() {
 	'use strict';
+
+//大数字缩短长度
+Number.prototype.bigNumberToString = function() {
+    let numTemp = this.valueOf();
+    if (!numTemp) return "0";
+    const grouping = Math.pow(10, 4);
+    const unit = ['', '万', '亿', '兆', '京', '垓'];
+    const numParts = [];
+    do {
+        numParts.push(numTemp % grouping);
+        numTemp = Math.floor(numTemp / grouping);
+    } while (numTemp > 0 && numParts.length < (unit.length - 1))
+    if (numTemp > 0) {
+        numParts.push(numTemp);
+    }
+    let numPartsStr = numParts.map((num, idx) => {
+        if (num > 0) {
+            return (num < 1e3 ? "零" : "") + num + unit[idx];
+        } else
+            return "零";
+    });
+
+    numPartsStr.reverse(); //反向
+    let outStr = numPartsStr.join("");
+    outStr = outStr.replace(/(^零+|零+$)/g, ''); //去除开头的零
+    outStr = outStr.replace(/零{2,}/g, '零'); //去除多个连续的零
+    return outStr;
+}
 
 	//监听head的加载，代码来源于 EhTagSyringe
 	const headLoaded = new Promise(function (resolve, reject) {
@@ -40,7 +68,7 @@
 
 	//head加载后添加国内的JQ源
 	headLoaded.then(function (head) {
-		let jq = document.createElement("script");
+		const jq = document.createElement("script");
 		jq.id = "user-jQuery";
 		jq.type = "text/javascript";
 		jq.innerHTML = GM_getResourceText("jquery");
@@ -48,19 +76,44 @@
 	})
 
 	//启动器
-	var bootstrap = function(){
+	const bootstrap = function(){
 		unsafeWindow.$('#StageInfo').bind('click cut copy paste', function(event) {
 			unsafeWindow.$('#StageInfo').unbind(); //调用jQ自身的去掉绑定
 		});
-		let styleDom = document.body.appendChild(document.createElement("style"));
-		styleDom.innerHTML = `* {
-	-webkit-touch-callout: unset !important;
-	-webkit-user-select: unset !important;
-	-khtml-user-select: unset !important;
-	-moz-user-select: unset !important;
-	-ms-user-select: unset !important;
-	user-select: unset !important;
-}`;
+		const styleDom = document.body.appendChild(document.createElement("style"));
+		styleDom.innerHTML = `
+		* {
+			-webkit-touch-callout: unset !important;
+			-webkit-user-select: unset !important;
+			-khtml-user-select: unset !important;
+			-moz-user-select: unset !important;
+			-ms-user-select: unset !important;
+			user-select: unset !important;
+		}`;
+		const stageDetail = document.body.querySelector("#StageInfo>table:nth-of-type(2)");
+		if (stageDetail)
+		{
+			const centerRows = stageDetail.tBodies[0].querySelectorAll(":scope>tr[align=\"center\"]:not(:first-child)");
+			let numberTds = [];
+			for (let tr of centerRows)
+			{
+				let tds = tr.querySelectorAll(":scope>td:not([rowspan])");
+				if (tds.length>5)
+				{
+					numberTds.push(tds[0]);
+					numberTds.push(tds[5]);
+				}
+			}
+			
+			for (let td of numberTds)
+			{
+				if (/[\d,\-]/g.test(td.textContent))
+				{
+					const num = parseInt(td.textContent.replace(/,/g,""));
+					td.textContent = num.bigNumberToString();
+				}
+			}
+		}
 	}
 
 	//加载document后执行启动器
